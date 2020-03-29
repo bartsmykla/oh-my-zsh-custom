1a6a18b2f84b4f4b380a609bd77af91a7d2a9d15
Bart Smykla <bartek@smykla.com>
2020-03-29 11:59:38 +0200

    Changed prompt theme
    Currently it looks like:
    [?GIT_DIRTY_ICON ][DIRECTORY_NAME]([GIT_BRANCH]#[GIT_HASH]) [ARROW_WHICH_CHANGES_SYMBOL_WHEN_DIFFERENT_USER]
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ main.zsh
@@ -0,0 +1,53 @@
+function git_is_dirty() {
+  local STATUS
+  local -a FLAGS
+  FLAGS=('--porcelain')
+  if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
+    if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
+      FLAGS+='--untracked-files=no'
+    fi
+    case "$GIT_STATUS_IGNORE_SUBMODULES" in
+      git)
+        # let git decide (this respects per-repo config in .gitmodules)
+        ;;
+      *)
+        # if unset: ignore dirty submodules
+        # other values are passed to --ignore-submodules
+        FLAGS+="--ignore-submodules=${GIT_STATUS_IGNORE_SUBMODULES:-dirty}"
+        ;;
+    esac
+    STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+  fi
+  echo ${STATUS}
+}
+
+# Outputs current branch info in prompt format
+function git_prompt_info() {
+  local ref
+  if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
+    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  fi
+}
+
+function git_current_user_symbol() {
+  local user_email="$(git config user.email 2>/dev/null)"
+  local symbol_priv="➜"
+  local symbol_work="❀"
+  local symbol
+  
+  case "${user_email}" in
+    bartek@smykla.com)
+      symbol="${symbol_priv}"
+      ;;
+    
+    bsmykla@vmware.com)
+      symbol="${symbol_work}"
+      ;;
+
+    *)
+  esac
+
+  echo "${symbol}"
+}
--- themes/robbyrussell.zsh-theme
+++ themes/robbyrussell.zsh-theme
@@ -1,14 +1,13 @@
-PROMPT='$(git_prompt_info)$(git_prompt_short_sha)'
+PROMPT='$(parse_git_dirty)'
 PROMPT+='%{$fg_bold[cyan]%}%c%{$reset_color%}'
-PROMPT+=' %(?:%{$fg_bold[green]%}➜:%{$fg_bold[red]%}➜) %{$reset_color%}'
+PROMPT+='($(git_prompt_info)$(git_prompt_short_sha))%{$reset_color%}'
+PROMPT+=' %(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})$(git_current_user_symbol)%{$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%} %{$fg[yellow]%}✗ "
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[yellow]%}✗ "
+ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$fg[blue]%}["
-ZSH_THEME_GIT_PROMPT_SHA_BEFORE+="%{$fg_bold[grey]%}"
+ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$fg_bold[grey]%}#"
 
-ZSH_THEME_GIT_PROMPT_SHA_AFTER="%{$reset_color%}%{$fg[blue]%}]"
-ZSH_THEME_GIT_PROMPT_SHA_AFTER+="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_SHA_AFTER="%{$reset_color%}"
